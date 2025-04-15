1ff568fa0247fbcd31271c14d711cbb43bd4d0af
GaganSingh2 <gks022004@gmail.com>
2025-04-16 02:18:05 +0530

    "Numbers in Data Type of JS"

---
+++ JavaScript/Data_types.js
@@ -0,0 +1,16 @@
+// Numbers
+a = 34
+console.log(typeof a)
+
+b = 2.4
+console.log(typeof b)
+
+c = -234
+console.log(typeof c)
+
+val = 0.999999999
+console.log(val)
+
+// if we cross the limit of the datatype then the value goes to nearest one's
+val = 0.99999999999999999
+console.log(val)
