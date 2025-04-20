ffcdea5c8d2e6e6642cf0b487421af05c12e71d3
GaganSingh2 <gks022004@gmail.com>
2025-04-21 00:37:00 +0530

    "Practice Qus and String"

--- JavaScript/Level_1/String.js
+++ JavaScript/Level_1/String.js
@@ -6,3 +6,12 @@ console.log(val);
 
 let res = 'This is "Gagan"';
 console.log(res);
+
+console.log(res[9]);  //access the character using index
+console.log(res.length); //find the length of string
+
+console.log("Gagan"[0]);
+
+// Concatenation
+let newRes = "Gagan"+" "+"Kumar";
+console.log(newRes);
--- JavaScript/Level_1/prac.js
+++ JavaScript/Level_1/prac.js
@@ -7,3 +7,23 @@ console.log(newVal);
 
 newVal = ++val;
 console.log(newVal);
+
+// Q1)Declare your name as  a string and print its length in Js.
+let name = "Gagan Kumar";
+console.log("Length of Name: "+name.length);
+
+// Q2)Declare your name as  a string and print its first character in Js.
+console.log("First Character is: "+name[0]);
+
+// Q3)Declare your name as  a string and print its last character in Js.
+console.log("Last Character is: "+name[name.length-1]);
+
+// Q4)Whats  the output
+console.log("apnaCollege"+123);
+
+// Q5)What are the length of empty string and string with a single space
+let empty = "";
+console.log("Length of Empty String: "+empty.length);
+
+let singleSpace = " ";
+console.log("Length of SignleSpace String: "+singleSpace.length);
