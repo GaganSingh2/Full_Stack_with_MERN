90eeffc7f2b81f675f3c06911950bc5039b9293d
GaganSingh2 <gks022004@gmail.com>
2025-04-25 00:26:03 +0530

    "Linking the Js file to HTML file and print the value on console"

---
+++ JavaScript/Level_2/linking_Of_html.js
@@ -0,0 +1,13 @@
+console.log("Radhey RAdhey");
+
+let num1 = 2345;
+let num2 = 23445;
+console.log("Sum is: ",num1+num2);
+
+let val = 23;
+if(val % 2==0){
+    console.log(val," is Even number.")
+}
+else{
+    console.log(val," is Odd number.")
+}
