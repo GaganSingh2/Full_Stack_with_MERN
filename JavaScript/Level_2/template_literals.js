cff73a32e3456360312f8cfe679a5aa1ad17ce14
GaganSingh2 <gks022004@gmail.com>
2025-04-25 00:40:30 +0530

    "Template Literauls in Js"

---
+++ JavaScript/Level_2/template_literals.js
@@ -0,0 +1,7 @@
+let phonePrice = 23000;
+let earBudsPrice = 1000;
+
+// let output = "The total price is: "+ (phonePrice + earBudsPrice)+" ruppes.";
+
+let output = `The total price is: ${phonePrice+earBudsPrice} rupees.`;
+console.log(output);
