a7f4855bb8cd55117dd83001e70e08c50a4a9aa9
GaganSingh2 <gks022004@gmail.com>
2025-04-25 17:31:27 +0530

    "Create a Alert and Prompt"

---
+++ JavaScript/Level_2/alert_prompt.js
@@ -0,0 +1,17 @@
+alert("Something went wrong!");
+
+console.error("We create a error message bidefault!")
+
+
+console.warn("We create a warning message bidefault!")
+
+// Prompt: we use to enter the value to user
+
+let firstName = prompt("Enter your first name: ")
+let lastName = prompt("Enter your first name: ")
+
+let mess = `Welcome ${firstName} ${lastName}!`;
+alert(mess);
+console.log(mess);
+// let name = prompt("Enter your Name: ");
+// console.log(`Your name is: ${name}`);
